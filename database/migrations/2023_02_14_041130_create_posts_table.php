c1f92c8144ec0892dbdb85929c49b2e44bd79444
ricky-blip <ricky.rinaldy77@gmail.com>
2023-02-14 13:48:29 +0700

    update membuat one to one, one to many

--- database/migrations/2023_02_14_041130_create_posts_table.php
+++ database/migrations/2023_02_14_041130_create_posts_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('category_id');
             $table->string('title');
             $table->string('author');
             $table->string('slug')-> unique();
